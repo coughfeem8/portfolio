11845716947c00b82d3b38dcf733557df856a964
Coughfeem8 <serjyan.des@gmail.com>
2023-11-06 01:55:18 -0700

    removed unneeded common module

--- src/app/modules/statistics/statistics.module.ts
+++ src/app/modules/statistics/statistics.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LandingComponent } from './pages/landing/landing.component';
 import { Route, RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Route[] = [
   {
@@ -21,7 +21,7 @@ const routes: Route[] = [
     LandingComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ]
 })
