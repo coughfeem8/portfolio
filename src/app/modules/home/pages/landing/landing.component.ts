15f9f0a6481a9ac2d604a509d2f7a9ade6e1f057
coughfeem8 <serjyan.des@gmail.com>
2023-12-01 17:16:43 -0700

    adding main layout for home and about for phones

--- src/app/modules/home/pages/landing/landing.component.ts
+++ src/app/modules/home/pages/landing/landing.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, skipWhile, takeUntil } from 'rxjs';
+import { Size } from 'src/app/modules/shared/models/layout';
+import { ResponsiveService } from 'src/app/modules/shared/services/responsive.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
-  constructor(private router: Router) {}
+export class LandingComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private responsive: ResponsiveService) {}
+
+  public isPhone: boolean= true;
+  public onDestroy$: Subject<boolean> = new Subject();
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+  ngOnInit(): void {
+    this.responsive.deviceInfo$
+    .pipe(
+      skipWhile((p) => p.size === null),
+      takeUntil(this.onDestroy$),
+    )
+    .subscribe((res) => {
+      this.isPhone = res.size === Size.PHONE;
+    });
+  }
 
   navigateTo(path: string) {
     this.router.navigate([path]);
